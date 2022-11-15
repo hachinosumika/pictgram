770710cc3b101086bf1b12f4a878462f29212dda
GUN-m <testtest@example.com>
2022-11-15 15:46:15 +0000

    Rails 04 課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+.[a-z]+\z/i }
+  
+VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX },
+                       length: { in: 8..32 }
+  
+  
+  has_secure_password
+  
 end
+
